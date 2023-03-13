67459cae4664cf692feb09c33398d338f814a499
John Muchovej <jmuchovej@users.noreply.github.com>
2023-03-13 13:09:12 -0400

    ⚰️ Remove unnecessary zstyles, aliases, completions

--- dot_config/zsh/conf.d/private_02-zstyles.zsh
+++ dot_config/zsh/conf.d/private_02-zstyles.zsh
@@ -1,12 +1,5 @@
 #!/usr/bin/env zsh
 
-zstyle :omz:plugins:ssh-agent agent-forwarding on
+zstyle ":omz:plugins:ssh-agent" agent-forwarding on
 
-# keys=("homelabs" "personal" "research")
-# identities=()
-# for key in $keys; do
-#     key="john+${key}@muchovej.com.pem"
-#     [ -f ${HOME}/.ssh/${key} ] && identities+=($key)
-# done
-# 
-# zstyle :omz:plugins:ssh-agent identities ${identities}
+zstyle ":omz:plugins:nvm" autoload yes
